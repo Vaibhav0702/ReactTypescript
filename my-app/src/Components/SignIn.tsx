9f3a151f34336e69661513cd8e987509880d54d3
Vaibhav <vaibhavginnalwar@gmail.com>
2024-09-27 14:20:25 +0530

    learn how to use typescript type with useState

--- my-app/src/Components/SignIn.tsx
+++ my-app/src/Components/SignIn.tsx
@@ -2,17 +2,29 @@ import React, { useState } from 'react'
 
 export default function SignIn() {
 
+    interface UserType{
+        name : string,
+        email : string
+    }
 
     const [isSignIn, setSignIn] = useState(false);
 
+    const [user , setUser] = useState<UserType | null>();  // set useState type as userTYpe or null
+
 
     const handleSignIn = () => {
-        setSignIn(true)
+        setSignIn(true);
+
+        setUser({
+            name : "vaibhav",
+            email : "vaibhav@mail.com"
+        })
     }
 
 
     const handleSignOut = () => {
-        setSignIn(false)
+        setSignIn(false);
+        setUser(null);
     }
 
 
@@ -26,6 +38,14 @@ export default function SignIn() {
             <button onClick={handleSignOut} >Sign out</button>
 
             <div> {isSignIn ? "SignIn" : "SignOut"}</div>
+
+
+            <div>
+                {user?.name} 
+            </div>
+            <div>
+                {user?.email}
+            </div>
 
         </div>
     )
