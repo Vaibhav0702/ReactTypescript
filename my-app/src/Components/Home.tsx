fffef3d40dd53d6a871fa9e17ed81b5941e46b24
Vaibhav <vaibhavginnalwar@gmail.com>
2024-09-27 13:12:09 +0530

    add basic props with type

---
+++ my-app/src/Components/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// type PropsType = {
+//     name : string,
+//     age : number,
+//     isEligible : boolean
+// }
+
+interface PropsType {
+    name: string,
+    age: number,
+    isEligible: boolean
+}
+
+
+export default function Home(props: PropsType) {
+    return (
+
+        <>
+            <div>Hello {props.name} , Welcom to the Page </div>
+            {props.isEligible ? <p>Your age is {props.age} and you are eligible to vote.</p> : <p>you Cannot vote.</p>}
+        </>
+    )
+}
